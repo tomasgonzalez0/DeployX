82a964e2446cb6d29d32293652c2335c3dd02ce4
Carolina Arango <carolinaarango1114665@correo.itm.edu.co>
2026-08-28 21:07:27 -0500

    crear GET /api/version

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetVersion()
+    {
+        return Ok(new { version = "1.0.0" });
+    }
+    
+}
