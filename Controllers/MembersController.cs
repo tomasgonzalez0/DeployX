e06d9af06deee5fd818bf3ef66d4c93de226e47e
Daniel Marín <danielmarinm0510@gmail.com>
2026-08-28 21:15:08 -0500

    crear GET /api/members con los nombres y apellidos completos reales.

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMembers()
+    {
+        var members = new[]
+        {
+            "Tomas Gonzalez Zapata",
+            "Daniel Felipe Marin Marin",
+            "Carolina Arango Escobar",
+            "Carla Juliana Giraldo Camacho",
+            "Samuel Cristobal Cuello Duque"
+        };
+        return Ok(members);
+    }
+}
