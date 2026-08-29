7e1296d042b72979b45c97eab02e85e993e9531e
samuelcuello05 <samuelcuello1116146@correo.itm.edu.co>
2026-08-28 20:44:57 -0500

    feat: add GetInfo API endpoint on Info Service

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InfoController : ControllerBase
+    { 
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok(new {description = "Práctica colaborativa con Git"});
+        }
+    }
+}
